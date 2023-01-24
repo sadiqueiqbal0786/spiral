d76df8a732c78f15b6f87ec61a40b6bd89cb1a48
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-24 22:36:36 +0530

    tough one

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,38 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] result = new int[rows][columns];
+        int count = 1;
+        int rowStart = 0;
+        int rowEnd = rows - 1;
+        int colStart = 0;
+        int colEnd = columns - 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            // Fill first row from left to right
+            for (int i = colStart; i <= colEnd; i++) {
+                result[rowStart][i] = count++;
+            }
+            rowStart++;
+            // Fill last column from top to bottom
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result[i][colEnd] = count++;
+            }
+            colEnd--;
+            // Fill last row from right to left
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result[rowEnd][i] = count++;
+                }
+                rowEnd--;
+            }
+            // Fill first column from bottom to top
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result[i][colStart] = count++;
+                }
+                colStart++;
+            }
+        }
+        return result;
     }
 }
